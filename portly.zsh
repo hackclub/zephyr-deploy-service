9530cba41627976ca04ee94bd3b0bca0dc24bdd8
Zachary Fogg <me@zfo.gg>
2021-07-08 17:09:44 -0400

    better portly commands

--- portly.zsh
+++ portly.zsh
@@ -72,8 +72,13 @@ function reserve_port() {
 local command="${1:-/dev/null}"
 
 if [[ "$command" == "print" ]]; then
-	(echo 'PORT' 'DOMAIN'; 
-	for port domain in ${(kv)RESERVED_PORTS[@]}; do
+	local first="${2:-domain}"
+	local reservations;
+	if [[ "$first" == "port" ]];
+	then reservations=(${(kv)RESERVED_PORTS[@]});
+	else reservations=(${(kv)RESERVED_DOMAINS[@]}); fi
+	([[ "$first" == "port" ]] && echo 'PORT' 'DOMAIN' || echo 'DOMAIN' 'PORT';
+	for port domain in ${(kv)reservations[@]}; do
 		printf '%s %s\n' "$port" "$domain"
 	done) | column -t
 elif [[ "$command" == "free_port" ]]; then
@@ -84,7 +89,7 @@ elif [[ "$command" == "reserve_port" ]]; then
 	reserve_port "$2"
 	echo "set port for $2=$RESERVED_DOMAINS[$2]"
 else
-	>&2 echo "no command selected (try 'print', 'reserve_port', or 'free_port')"
+	>&2 echo "no command selected (try 'print', 'reserve_port \$domain', or 'free_port \$domain')"
 fi
 # }}}
 
