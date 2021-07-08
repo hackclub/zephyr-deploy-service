a2482b5972166cac79a940ee06775451da89206c
Rishi Kothari <hey@rishi.cx>
2021-07-08 15:44:20 -0400

    added portly :D

--- portly.zsh
+++ portly.zsh
@@ -2,12 +2,13 @@
 
 local script=$0:A
 local scriptdir=`dirname $script`
-echo script $scriptdir scriptdir $scriptdir
+#echo script $scriptdir scriptdir $scriptdir
 
 local rootdir=${1:-$scriptdir/test/portly}
-echo rootdir $rootdir
+#echo rootdir $rootdir
 
 local testdir="$scriptdir"/test/portly
+#echo testdir $testdir
 
 function _get_reserved_ports() {
 	grep -Po '.*PORT=\K(\d+)' "$testdir"/*.zephyr/.env | sed 's/:\([0-9]\+\)/\t\1/'
@@ -15,13 +16,15 @@ function _get_reserved_ports() {
 
 function get_reserved_ports() {
 	local _reserved_ports=("${(@f)$(_get_reserved_ports)}")
-	local reserved_ports=()
+	declare -A reserved_ports
 	for line in ${_reserved_ports[@]}; do
 		while IFS=$'\t' read -A resv_port; do
-			reserved_ports+=(${resv_port[1]}, ${resv_port[2]})
+			reserved_ports[${resv_port[2]}]=${resv_port[1]:r:t}
 		done < <(echo "$line")
 	done
-	echo "${reserved_ports[@]}" | tr ',' '\n'
+	for port domain in ${(kv)reserved_ports[@]}; do
+		printf '%s %s\n' "$port" "$domain";
+	done
 }
 
 get_reserved_ports
