6b0c63cf26a3f1619395c5334dd431eb31c4622a
Max Wofford <max@maxwofford.com>
2021-07-10 13:49:18 -0400

    Spec out deleted.js

--- deleted.js
+++ deleted.js
@@ -1,1 +1,14 @@
+const {existsSync} = require('fs')
+
+const [, , folder, methods, file] = process.argv
 console.log(process.argv)
+
+// Note: Removing the /opt/zephyr/watcher/repo folder is intentionally left out of this to prevent accidental deletions from removing history
+
+// Remove systemctl files
+
+// Reload systemctl
+
+// Remove DNS records
+
+// Remove port from list of assigned ports
