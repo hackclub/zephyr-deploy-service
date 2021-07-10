348f487f162430229b3d865e6bbace274e609b2a
Max Wofford <max@maxwofford.com>
2021-07-10 15:12:00 -0400

    Rewrite port reservation switch

--- deploy.js
+++ deploy.js
@@ -3,7 +3,7 @@ const { parse } = require('envfile')
 const fetch = require('sync-fetch')
 const { compile } = require("handlebars");
 const { execSync } = require('child_process')
-const { reservePort } = require('./ports')
+const { getPortFromDomain } = require('./ports')
 const path = require('path');
 
 const [, , folder, methods, file] = process.argv;
@@ -71,70 +71,45 @@ const addRecord = (obj) => fetch("http://10.10.8.210:9191/api/v1/servers/localho
 
 // We need to check only on file creation, not folders, and we can emulate first-tier checking
 if (!methods.includes("ISDIR") && folder.endsWith(".zephyr/")) {
-
     switch (file) {
-        case "entry.sh":
-            const files = readdirSync(folder)
-            if (files.includes('.env')) {
-                const env = parse(readFileSync(path.join(folder, '.env')))
-                const port = env.PORT || env.port || undefined
-                if (port) {
-                    const dynamicConfTemplate = compile(readFileSync('/opt/zephyr/watcher/dynamic_conf_template.hbs', 'utf8'))
-                    const name = folder.split("/")[3]
-
-                    writeFileSync(`/etc/nginx/sites-enabled/${name}.conf`, dynamicConfTemplate({
-                        site: name,
-                        port
-                    }))
-
-                    addRecord({
-                        name,
-                        type: "A",
-                        content: "10.10.8.210"
-                    })
-
-                }
-            } else {
-                const name = folder.split("/")[3]
-                console.log("[warn] no port found yet.")
-                // Generate port file
-                writeFileSync(path.join(folder, '.env'), "# Created by porter (/opt/zephyr/watcher/ports.js")
-                const port = reservePort(name)
-                const dynamicConfTemplate = compile(readFileSync('/opt/zephyr/watcher/dynamic_conf_template.hbs', 'utf8'))
-
-
-                writeFileSync(`/etc/nginx/sites-enabled/${name}.conf`, dynamicConfTemplate({
-                    site: name,
-                    port
-                }))
-
-                addRecord({
-                    name,
-                    type: "A",
-                    content: "10.10.8.210"
-                })
+        case "entrypoint.sh":
+            if (readdirSync(folder).includes("index.html")) {
+                console.log('[warn] index.html already exists in folder, ignoring entrypoint.sh...')
+                break
             }
+            const dynamicConfTemplate = compile(readFileSync('/opt/zephyr/watcher/dynamic_conf_template.hbs', 'utf8'))
+            const name = folder.split("/")[3]
+
+            writeFileSync(`/etc/nginx/sites-enabled/${name}.conf`, dynamicConfTemplate({
+                site: name,
+                port: getPortFromDomain(name)
+            }))
+
+            addRecord({
+                name,
+                type: "A",
+                content: "10.10.8.210"
+            })
+            break
         case "index.html":
-            const fs = readdirSync(folder)
-            if (fs.includes("entry.sh")) {
-                console.log(`[log] index.html found in a dynamic directory, skipping process.`)
-            } else {
-                console.log(`[log] loading static file loader`)
-                const staticConfTemplate = compile(readFileSync('/opt/zephyr/watcher/static_conf_template.hbs', 'utf8'))
-                const name = folder.split("/")[3]
-
-                writeFileSync(`/etc/nginx/sites-enabled/${name}.conf`, staticConfTemplate({
-                    site: name
-                }))
-
-                addRecord({
-                    name,
-                    type: "A",
-                    content: "10.10.8.210"
-                })
-
+            if (readdirSync(folder).includes("entrypoint.sh")) {
+                console.log('[warn] entrypoint.sh already exists in folder, ignoring index.html...')
+                break
             }
+            console.log(`[log] loading static file loader`)
+            const staticConfTemplate = compile(readFileSync('/opt/zephyr/watcher/static_conf_template.hbs', 'utf8'))
+            const name = folder.split("/")[3]
 
+            writeFileSync(`/etc/nginx/sites-enabled/${name}.conf`, staticConfTemplate({
+                site: name
+            }))
+
+            addRecord({
+                name,
+                type: "A",
+                content: "10.10.8.210"
+            })
+            break
         default:
             console.log("[warn] could not identify type of directory.")
             break
