8d23f05ed0e9dd69a14d2518b971e70eb1132185
Rishi Kothari <itsrishikothari@gmail.com>
2021-07-11 15:25:24 -0400

    add exclusion

--- index.bash
+++ index.bash
@@ -2,7 +2,7 @@
 
 while true; do
 
-    inotifywait -e create -r /opt/zephyrnet | while read dir action structure; do
+    inotifywait -e create -r /opt/zephyrnet --exclude 'node_modules/' | while read dir action structure; do
         echo "inotifywait 'create' with arguments: $dir $action $structure"
         sudo node /opt/zephyr/watcher/deploy.js $dir $action $structure
     done
