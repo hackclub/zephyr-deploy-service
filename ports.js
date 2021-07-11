6e96985c41d83a61ef204d024f9ea71f2824e5fe
Rishi Kothari <itsrishikothari@gmail.com>
2021-07-11 13:30:53 -0400

    ports refactors

--- ports.js
+++ ports.js
@@ -1,12 +1,11 @@
 const fs = require("fs")
 const { join } = require("path")
-const envfile = require('envfile')
 
 const dir = "/opt/zephyrnet"
-const ports_file = "/dev/"
+const ports_file = "/opt/ports/ports.json"
 
  const getPorts = () => {
-    return JSON.parse(fs.readFileSync("./ports.json"))
+    return JSON.parse(fs.readFileSync(ports_file))
 }
 
 const random = (min, max) => { // min and max included 
@@ -16,7 +15,7 @@ const random = (min, max) => { // min and max included
  const genPort = (json) => {
     const port = random(1000, 9999)
     console.log(port)
-    const ports = json || Object.entries(JSON.parse(fs.readFileSync("./ports.json")))
+    const ports = json || Object.entries(JSON.parse(fs.readFileSync(ports_file)))
     if (port in ports) {
         return genPort(ports)
     } else {
@@ -26,13 +25,9 @@ const random = (min, max) => { // min and max included
 
 const reservePort = (domain) => {
     const port = genPort()
-
-    const env = fs.readFileSync(join(dir, domain, '.env'))
-    env.PORT = port
-    fs.writeFileSync(join(dir, domain, '.env'), envfile.stringify(env))
     const ports = getPorts()
     ports[domain] = port
-    fs.writeFileSync("./ports.json", JSON.stringify(ports, null, 2))
+    fs.writeFileSync(ports_file, JSON.stringify(ports, null, 2))
 
     return port
 }
@@ -42,22 +37,16 @@ const getPort = (domain) => {
     if (port) {
         return port    
     } else {
-        reservePort(domain)
-        return getPorts()[domain]
+        return reservePort(domain)
     }
     
 }
 
 const freePort = (domain) => {
     let ports = getPorts()
-    if (ports[domain] && fs.existsSync(join(dir, domain, '.env'))) {
-        let { PORT, ...env } = envfile.parse(fs.readFileSync(join(dir, domain, '.env')));
-        // remove env[domain];
-        console.log(env)
-        fs.writeFileSync(join(dir, domain, '.env'), envfile.stringify(env))
-        
+    if (ports[domain]) {
         let { [domain]: cacheRemoved, ...portsUpdate } = ports;
-        fs.writeFileSync("./ports.json", JSON.stringify(portsUpdate, null, 2))
+        fs.writeFileSync(ports_file, JSON.stringify(portsUpdate, null, 2))
 
     }
 
