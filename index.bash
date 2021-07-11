0e4190aca07de2cd4c5b95ff358e38d55a868862
Max Wofford <max@maxwofford.com>
2021-07-11 15:08:40 -0400

    Add missing 'done'

--- index.bash
+++ index.bash
@@ -10,5 +10,6 @@ while true; do
     inotifywait -e delete,DELETE -r /opt/zephyrnet | while read dir action structure; do
         echo "inotifywait 'delete' with arguments: $dir $action $structure"
         sudo node /opt/zephyr/watcher/deleted.js $dir $action $structure
+    done
 
 done
