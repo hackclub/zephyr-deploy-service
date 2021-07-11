c1d44ac5203b50d4e5886fba290f8f1df75c9e28
Max Wofford <max@maxwofford.com>
2021-07-11 13:26:33 -0400

    Add ports file writing

--- ports.js
+++ ports.js
@@ -1,4 +1,4 @@
-const { existsSync } = require('fs')
+const { existsSync, writeFileSync } = require('fs')
 const lockfile = require('proper-lockfile')
 
 const portsDir = "./ports"
@@ -14,13 +14,25 @@ const portIsAllocated = (port) => {
 }
 
 const getPort = (domain) => {
+    console.log('Trying to get a domain for', domain)
+
+    console.log(`...adding lockfile on port directory`)
     const lock = lockfile.lockSync(portsDir)
 
     const randomPort = Math.random() * (65535 - 1024) + 1024 // port range is 1024-65535
+    console.log(`...trying port '${randomPort}'`)
     if (portIsInUse(randomPort) || portIsAllocated(randomPort)) {
+        console.log(`...port ${randomPort} is not available`)
         return getPort(domain)
     } else {
+        console.log(`...port ${randomPort} is available`)
+
+        console.log(`...writing port allocation to /opt/zephyr/watcher/ports/${randomPort}`)
+        writeFileSync(`${portsDir}/${randomPort}`, domain)
+
+        console.log('...removing lockfile on port directory')
         lock.unlockSync(portsDir)
+
         return randomPort
     }
 }
