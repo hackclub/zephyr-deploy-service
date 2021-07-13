2ba56976aded29788cd0c32a8fb49a4b9bcff013
Max Wofford <max@maxwofford.com>
2021-07-13 17:28:29 -0400

    Log modify & move events

--- index.bash
+++ index.bash
@@ -7,6 +7,14 @@ while true; do
         sudo node /opt/zephyr/watcher/deploy.js $dir $action $structure
     done
 
+    inotifywait -e modify -r /opt/zephyrnet --exclude 'node_modules/' | while read dir action structure; do
+        echo "inotifywait 'modify' with arguments: $dir $action $structure"
+    done
+
+    inotifywait -e move -r /opt/zephyrnet --exclude 'node_modules/' | while read dir action structure; do
+        echo "inotifywait 'move' with arguments: $dir $action $structure"
+    done
+
     # inotifywait -e delete,DELETE -r /opt/zephyrnet | while read dir action structure; do
     #     echo "inotifywait 'delete' with arguments: $dir $action $structure"
     #     sudo node /opt/zephyr/watcher/deleted.js $dir $action $structure
