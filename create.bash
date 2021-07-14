53e9d89b7904ab9d9495594c4b5d90576822ef58
Rishi Kothari <itsrishikothari@gmail.com>
2021-07-14 11:29:31 -0400

    create bash

---
+++ create.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+inotifywait -e create -m -r /opt/zephyrnet --exclude '(node_modules/|.git/)' | while read dir action structure; do
+    echo "inotifywait 'create' with arguments: $dir $action $structure"
+    sudo node /opt/zephyr/watcher/deploy.js $dir $action $structure
+done
