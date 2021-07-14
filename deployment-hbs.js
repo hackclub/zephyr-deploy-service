85ce9a7f1e6b4b4d7aa02c4fe5e8da08ddbc044a
Rishi Kothari <itsrishikothari@gmail.com>
2021-07-14 13:43:42 -0400

    stuff

--- deployment-hbs.js
+++ deployment-hbs.js
@@ -3,7 +3,7 @@ const { compile } = require("handlebars")
 
 const [,,site, port] = process.argv
 
-const  template = compile(readFileSync('./systemd-unit-template.service.hbs').toString())
+const  template = compile(readFileSync('/opt/zephyr/watcher/systemd-unit-template.service.hbs').toString())
 
 console.log(template({
     site,
