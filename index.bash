f402fd4a01940f03f45e3165b962608ffc9aeb10
Max Wofford <max@maxwofford.com>
2021-07-14 09:19:18 -0400

    Run inotifywait constantly

--- index.bash
+++ index.bash
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 while true; do
-
-    inotifywait -e create -r /opt/zephyrnet --exclude 'node_modules/' | while read dir action structure; do
+    inotifywait -m -e create -r /opt/zephyrnet --exclude 'node_modules/' | while read dir action structure; do
         echo "inotifywait 'create' with arguments: $dir $action $structure"
         sudo node /opt/zephyr/watcher/deploy.js $dir $action $structure
     done
